6b462df42d4add75e6e92e6dcdbd377cc713cee2
Admin <Admin@CHETUIWK597>
2022-03-07 19:08:59 +0530

    Add project files.

---
+++ Dbconnection/Migrations/20220307095339_y1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dbconnection.Migrations
+{
+    public partial class y1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Dbconnection/Models/EmployeeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dbconnection.Models;
+namespace Dbconnection.Models
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext dbContext;
+
+        public EmployeeController(ApplicationDbContext dbContext)
+
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Employee> emps =
+                dbContext.Employees.ToList();
+            return View(emps);
+
+        }
+
+        public IActionResult create()
+            {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult create(Employee emp)
+        {
+            dbContext.Employees.Add(emp);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var emp = dbContext.Employees.SingleOrDefault(e => e.Id == id);
+            if(emp!=null)
+            {
+                dbContext.Employees.Remove(emp);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            
+        }
+       
+        public IActionResult Edit(int id)
+        {
+            var emp = dbContext.Employees.SingleOrDefault(e => e.Id == id);
+            return View(emp);
+        }
+        [HttpPost]
+        public IActionResult Edit(Employee emp)
+        {
+            dbContext.Employees.Update(emp);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
